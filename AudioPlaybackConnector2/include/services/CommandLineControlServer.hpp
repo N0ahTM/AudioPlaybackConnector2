2cd91277c4386bb941c340330c7fc5b1f41e00d6
Noah Meyer <nmstorbeck@gmail.com>
2026-07-02 22:56:24 -0400

    Keep control server responsive during commands

--- AudioPlaybackConnector2/include/services/CommandLineControlServer.hpp
+++ AudioPlaybackConnector2/include/services/CommandLineControlServer.hpp
@@ -2,8 +2,12 @@
 
 #include <control/CommandProtocol.hpp>
 
+#include <wil/resource.h>
+
 #include <atomic>
+#include <condition_variable>
 #include <functional>
+#include <mutex>
 #include <string>
 #include <thread>
 
@@ -18,10 +22,15 @@ public:
 
 private:
     void ListenLoop() noexcept;
+    void DispatchClient(wil::unique_hfile pipe) noexcept;
     void HandleClient(HANDLE pipe) noexcept;
 
     std::atomic<bool> m_running = false;
+    std::atomic<uint32_t> m_activeClients = 0;
     std::thread m_thread;
+    std::mutex m_handlerMutex;
+    std::mutex m_activeClientsMutex;
+    std::condition_variable m_activeClientsCv;
     Handler m_handler;
     std::wstring m_pipeName;
 };
